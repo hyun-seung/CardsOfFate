fd2eb2bb7e328bd2ac7b410dd4aa6752e62efa83
hyunseung <ardimento22@naver.com>
2025-05-06 01:04:15 +0900

    init Project

---
+++ src/main/java/com/sp/cof/CardsOfFateApplication.java
@@ -0,0 +1,13 @@
+package com.sp.cof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsOfFateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsOfFateApplication.class, args);
+	}
+
+}
