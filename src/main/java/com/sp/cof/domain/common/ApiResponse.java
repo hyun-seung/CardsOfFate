21cd191cf91e464e1a8bd27ba000e32bdc8810ed
hyunseung <ardimento22@naver.com>
2025-05-21 01:04:03 +0900

    [REFACTOR] ApiResponse에서 null 필드 응답 제거를 위한 직렬화 설정 추가

--- src/main/java/com/sp/cof/domain/common/ApiResponse.java
+++ src/main/java/com/sp/cof/domain/common/ApiResponse.java
@@ -1,8 +1,10 @@
 package com.sp.cof.domain.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
     private final int status;
