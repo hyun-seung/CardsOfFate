5670596f0ae9a2f8f8eb17624785fcacfaa4c7b2
hyunseung <ardimento22@naver.com>
2025-06-10 23:00:16 +0900

    [CHORE] 로그백 설정 최적화 및 테스트 환경 분리

--- src/main/java/com/sp/cof/CardsOfFateApplication.java
+++ src/main/java/com/sp/cof/CardsOfFateApplication.java
@@ -1,13 +1,24 @@
 package com.sp.cof;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CardsOfFateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardsOfFateApplication.class, args);
+
+		log.info("\n===================================="
+			   + "\n                                    "
+			   + "\n   CoF APP                          "
+			   + "\n                                    "
+			   + "\n===================================="
+		);
 	}
 
 }
